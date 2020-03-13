d3edff4ec62b2a861a87c584404cc6d3d25521e5
Erick <ecanals07@gmail.com>
2020-03-13 10:52:31 -0400

    made javis sleep for random times to avoid detection

--- jarvis.py
+++ jarvis.py
@@ -10,34 +10,32 @@ import random
 
 class Jarvis:
     def __init__(self, user, password):
-        try:
-            self.user_name = user
-            self.user_password = password
-            self.driver = webdriver.Chrome(
-                executable_path='./chrome/chromedriver')
-            self.actions = ActionChains(self.driver)
-        except:
-            pass
+
+        self.user_name = user
+        self.user_password = password
+        self.driver = webdriver.Chrome(
+            executable_path='./chrome/chromedriver')
+        self.actions = ActionChains(self.driver)
 
     def login(self):
         try:
             self.driver.get(login_url)
-            sleep(2)
+            sleep(random.choice(range(1, 4)))
 
             self.driver.find_element_by_name('username').send_keys(user_name)
-            sleep(1)
+            sleep(random.choice(range(1, 4)))
 
             self.driver.find_element_by_name(
                 'password').send_keys(user_password)
-            sleep(1)
+            sleep(random.choice(range(1, 3)))
 
             self.driver.find_element_by_xpath(
                 '//button[@type="submit"]').click()
-            sleep(5)
+            sleep(random.choice(range(4, 6)))
 
             self.driver.find_element_by_xpath(
                 '//button[contains(text(), "Not Now")]').click()
-            sleep(1)
+            sleep(random.choice(range(1, 3)))
 
         except:
             pass
@@ -45,23 +43,23 @@ class Jarvis:
     def explore(self, tags, comments, num_pics):
         try:
             self.driver.get(tags_url + random.choice(tags))
-            sleep(2)
+            sleep(random.choice(range(1, 4)))
 
             self.driver.find_elements_by_class_name('_9AhH0')[0].click()
-            sleep(2)
+            sleep(random.choice(range(1, 4)))
 
             for i in range(num_pics):
                 try:
                     self.driver.find_elements_by_class_name('wpO6b')[0].click()
-                    sleep(2)
+                    sleep(random.choice(range(1, 4)))
 
                     self.driver.find_element_by_class_name('Ypffh').click()
                     self.driver.find_element_by_class_name('Ypffh').send_keys(
-                        random.choice(comments) + Keys.ENTER)
-                    sleep(5)
+                        comments[random.choice(comments)] + Keys.ENTER)
+                    sleep(random.choice(range(5, 7)))
 
                     self.driver.find_element_by_link_text('Next').click()
-                    sleep(2)
+                    sleep(random.choice(range(1, 4)))
                 except:
                     pass
         except:
